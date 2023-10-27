beba93d26cfaf4b4d6b30b9f64d1fb590e2ac177
Radek Vala <radek.www@gmail.com>
2023-10-27 14:49:01 +0200

    Translator Page UI added Tabs UI changed

--- src/app/translator/translator.page.ts
+++ src/app/translator/translator.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TranslatorPage implements OnInit {
 
+  userInput: String = ''
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  translateClicked() {
+    console.log(this.userInput)
   }
 
 }
